f2768f537773547d6b4c20318135ab98f3338f74
Sourav Kumar <codey@codey-2.local>
2026-03-06 23:44:42 +0530

    feat: implement spring cloud server and client config

--- additional/config-management/config/src/main/java/com/ecommerce/config/controllers/BuildInfoController.java
+++ additional/config-management/config/src/main/java/com/ecommerce/config/controllers/BuildInfoController.java
@@ -1,23 +1,33 @@
 package com.ecommerce.config.controllers;
 
-import com.ecommerce.config.services.BuildServiceImpl;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 @RequestMapping("/api/v1/config")
-@AllArgsConstructor
 public class BuildInfoController {
-    private BuildServiceImpl buildService;
+    @Value("${build.id:default}")
+    private String buildId;
+
+    @Value("${build.version:default}")
+    private String buildVersion;
+
+    @Value("${build.name:default}")
+    private String buildName;
+
+    @Value("${build.type:default}")
+    private String buildType;
 
     @GetMapping("/build-info")
     public String getBuildInfo() {
-        System.out.println("Build Info: " + buildService);
         return
-                "Build Id: " + buildService.getBuildId()
-                        + "\n Build Version: " + buildService.getBuildVersion()
-                        + "\n Build Name: " + buildService.getBuildName();
+                "Build Id: " + buildId + ",\n"
+                        + "Build Version: " + buildVersion + ",\n"
+                        + "Build Name: " + buildName+ ",\n"
+                        + "Build Type: " + buildType + "\n";
     }
 }
