81bab4f159cb4b9b9b187abe636f03c97841dcfe
Sourav Kumar <codey@codey-2.local>
2026-03-20 01:57:57 +0530

    impl: add api gateway

--- order/src/main/java/com/ecommerce/order/controllers/CartController.java
+++ order/src/main/java/com/ecommerce/order/controllers/CartController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @Slf4j
 public class CartController {
