2205710981c4c71dbf7dc2128365ef37893c1d1e
Sourav Kumar <codey@codey-2.local>
2026-03-20 00:15:04 +0530

    feat: add zipkin, grafana, and prometheus for tracing and logging

--- order/src/main/java/com/ecommerce/order/config/RestClientBaseConfig.java
+++ order/src/main/java/com/ecommerce/order/config/RestClientBaseConfig.java
@@ -1,10 +1,15 @@
 package com.ecommerce.order.config;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 
 /***
@@ -14,17 +19,52 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientBaseConfig {
 
+    @Autowired(required = false)
+    private ObservationRegistry observationRegistry;
+
+    @Autowired(required = false)
+    private Tracer tracer;
+
+    @Autowired(required = false)
+    private Propagator propagator;
+
     @Bean
     @Primary
     public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+        RestClient.Builder builder = RestClient.builder();
+
+        if(observationRegistry != null) {
+            builder.requestInterceptor(createTracingInterceptor());
+        }
+
+        return builder;
     }
 
     @Bean
     @LoadBalanced
     @Qualifier("lbRestClientBuilder")
     public RestClient.Builder lbRestClientBuilder() {
-        return RestClient.builder();
+        RestClient.Builder builder = RestClient.builder();
+
+        if(observationRegistry != null) {
+            builder.requestInterceptor(createTracingInterceptor());
+        }
+
+        return builder;
+    }
+
+    private ClientHttpRequestInterceptor createTracingInterceptor() {
+        return ((req, body, execution) -> {
+            if(tracer != null && propagator != null && tracer.currentSpan() != null) {
+                propagator.inject(tracer.currentTraceContext().context(),
+                        req.getHeaders(),
+                        (carrier, key, value) -> {
+                            assert carrier != null;
+                            carrier.add(key, value);
+                        });
+            }
+            return execution.execute(req, body);
+        });
     }
 
 }
